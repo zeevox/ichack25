5283ac31c813f9924ab7ac09b7bcd3c10e5f2f9e
Timothy Langer <zeevox.dev@gmail.com>
2025-02-01 19:01:32 +0000

    refactor: extract markdown in separate file

--- content.ts
+++ content.ts
@@ -1,44 +1,20 @@
-// content.ts
-import { Readability } from "@mozilla/readability"
-import TurndownService from "turndown"
+import { extractArticleMarkdown } from "~lib/extract"
 
 // Export config so Plasmo knows on which pages to run this script.
 export const config = {
-    matches: ["<all_urls>"],
-    run_at: "document_idle"
-}
-
-// Function to extract and log the article markdown.
-const extractArticleMarkdown = () => {
-    // Use DOMParser to parse a cloned version of the document's HTML.
-    const parser = new DOMParser()
-    const docClone = parser.parseFromString(
-        document.documentElement.outerHTML,
-        "text/html"
-    )
-
-    const reader = new Readability(docClone)
-    const article = reader.parse()
-
-    if (article) {
-        // Convert the extracted article HTML to Markdown.
-        const turndownService = new TurndownService()
-        const markdown = turndownService.turndown(article.content)
-
-        console.log(markdown)
-    } else {
-        console.log("No article content was found by Readability.")
-    }
+  matches: ["<all_urls>"],
+  run_at: "document_idle"
 }
 
 // Run the extraction function once the DOM is loaded.
 if (
-    document.readyState === "complete" ||
-    document.readyState === "interactive"
+  document.readyState === "complete" ||
+  document.readyState === "interactive"
 ) {
-    extractArticleMarkdown()
+  let markdownContent = extractArticleMarkdown()
+  console.log(markdownContent);
 } else {
-    document.addEventListener("DOMContentLoaded", extractArticleMarkdown)
+  document.addEventListener("DOMContentLoaded", extractArticleMarkdown)
 }
 
 // Ensure this file is treated as a module.
---
+++ lib/extract.ts
@@ -0,0 +1,23 @@
+import { Readability } from "@mozilla/readability"
+import TurndownService from "turndown"
+
+export const extractArticleMarkdown: () => string = () => {
+  // Use DOMParser to parse a cloned version of the document's HTML.
+  const parser = new DOMParser()
+  const docClone = parser.parseFromString(
+    document.documentElement.outerHTML,
+    "text/html"
+  )
+
+  const reader = new Readability(docClone)
+  const article = reader.parse()
+
+  if (article) {
+    // Convert the extracted article HTML to Markdown.
+    const turndownService = new TurndownService()
+    return turndownService.turndown(article.content)
+  } else {
+    console.error("No article content was found by Readability.")
+    return null
+  }
+}
