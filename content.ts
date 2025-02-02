fd387389a75f448e817fbbfdb4dab7d710c82952
Yongzhe K Li <yongzhe.li22@imperial.ac.uk>
2025-02-02 11:27:30 +0000

    Added BiasShield to loading bar

--- content.ts
+++ content.ts
@@ -138,7 +138,7 @@ async function main() {
     loadingContainer.appendChild(loadingLogo);
 
     const loadingText = document.createElement("div");
-    loadingText.innerText = "Loading bias report...";
+    loadingText.innerText = "BiasShield: loading bias report...";
     loadingContainer.appendChild(loadingText);
 
     document.body.appendChild(loadingContainer);
@@ -147,7 +147,7 @@ async function main() {
 
     const url = response["url"]
     const stance = response["stance"]
-    loadingText.innerHTML = `${stance}<br><br><a style="text-decoration: underline" href="${url}" target="_blank">Read alternative perspectives</a>`
+    loadingText.innerHTML = `<br>${stance}<br><br><a style="text-decoration: underline" href="${url}" target="_blank">Read alternative perspectives</a>`
 
     const biasedLanguageAnalysis = response["phrases"]
     console.log(biasedLanguageAnalysis)
